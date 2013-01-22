131f79009313ae0a696f594ff4516dfded996d38
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-01-22 18:27:45 +0900

    RGBの順に全面を光らせる

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 ARDUINO := /usr/share/arduino
 BOARD := uno
 SOURCES := arduino_puyopuyo.ino
-LIBRARIES := Tlc5940
+LIBRARIES := Tlc5940 SNESpad
 include ./arduino.mk
--- arduino_puyopuyo.ino
+++ arduino_puyopuyo.ino
@@ -1,16 +1,27 @@
 #include <Tlc5940.h>
+/* #include <SNESpad.h> */
+/* SNESpad nintendo = SNESpad(6, 5, 7); */
+/* byte state = 0; */
 
-//#define NUM_TLCS 1
+int i, j;
 
 void setup() {
   Tlc.init();
+
+  /* Serial.begin(9600); */
 }
 
 void loop() {
-  for (int i = 0; i < NUM_TLCS*16; i++) {
+  /* state = nintendo.buttons(); */
+  /* Serial.println(~state, BIN); */
+  /* delay(1000); */
+
+  for (j = 0; j < 3; j++) {
     Tlc.clear();
-    Tlc.set(i, 1000);
+    for (i = j; i < NUM_TLCS * 16; i += 3) {
+      Tlc.set(i, 1000);
+    }
     Tlc.update();
-    delay(100);
+    delay(2000);
   }
 }
