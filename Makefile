a557de207527aa19b41eba674fffb5444cb748a3
Shonosuke ISHIWATARI <w@sshoi.com>
2013-01-29 17:30:33 +0900

    graphic.cppとヘッダファイルを編集

--- Makefile
+++ Makefile
@@ -1,11 +1,21 @@
-ARDUINO := /usr/share/arduino
+UNAME := $(shell uname)
+
+ifeq ($(UNAME),Darwin) #MacOSX
+	ARDUINO := /Applications/Arduino.app/Contents/Resources/Java
+	ARDUINODIR := $(ARDUINO)
+else #Linux
+	ARDUINO := /usr/share/arduino
+endif
+
+
 BOARD := uno
-SOURCES := arduino_puyopuyo.ino LED.cpp game.cpp
+SOURCES := arduino_puyopuyo.ino LED.cpp game.cpp graphic.cpp
 LIBRARIES := Tlc5940 SNESpad
 include ./arduino.mk
 
 .PHONY: check-syntax
-INCLUDE := -I/usr/share/arduino/hardware/arduino/cores/arduino
+
+INCLUDE := -I$(ARDUINO)/hardware/arduino/cores/arduino
 CFLAGS := -Wall -Wextra $(INCLUDE)
 
 check-syntax:
--- graphic.cpp
+++ graphic.cpp
@@ -1,4 +1,90 @@
 #include "graphic.h"
+#include "LED.h"
+#include "arduino.h"
 
-Graphic::Graphic() {
+
+const bool Graphic::One[12][6] = {
+	{0,0,0,0,0,0},
+	{0,0,0,1,0,0},
+	{0,0,1,1,0,0},
+	{0,1,0,1,0,0},
+	{0,0,0,1,0,0},
+	{0,0,0,1,0,0},
+	{0,0,0,1,0,0},
+	{0,0,0,1,0,0},
+	{0,0,0,1,0,0},
+	{0,0,0,1,0,0},
+	{0,1,1,1,1,0},
+	{0,0,0,0,0,0}
+};
+
+const bool Graphic::Two[12][6] = {
+	{0,0,0,0,0,0},
+	{0,0,1,1,0,0},
+	{0,1,0,0,1,0},
+	{0,1,0,0,1,0},
+	{0,0,0,0,1,0},
+	{0,0,0,0,1,0},
+	{0,0,0,1,0,0},
+	{0,0,0,1,0,0},
+	{0,0,1,0,0,0},
+	{0,1,0,0,0,0},
+	{0,1,1,1,1,0},
+	{0,0,0,0,0,0}
+};
+
+const bool Graphic::Three[12][6] = {
+	{0,0,0,0,0,0},
+	{0,0,1,1,0,0},
+	{0,1,0,0,1,0},
+	{0,1,0,0,1,0},
+	{0,0,0,0,1,0},
+	{0,0,0,0,1,0},
+	{0,0,1,1,0,0},
+	{0,0,0,0,1,0},
+	{0,1,0,0,1,0},
+	{0,1,0,0,1,0},
+	{0,0,1,1,0,0},
+	{0,0,0,0,0,0}
+};
+
+
+void Graphic::CountDown(LED led) {
+
+	led.ClearAll();
+	for (int x = 0; x < led.kWidth; x++){
+		for (int y = 0; y < led.kHeight; y++){
+			if(Three[y][x]){
+				led.SetColor(x, y ,CYAN);
+			}
+		}
+	}
+	led.Update();
+	delay(1000);
+	
+	led.ClearAll();
+	for (int x = 0; x < led.kWidth; x++){
+		for (int y = 0; y < led.kHeight; y++){
+			if(Two[y][x]){
+				led.SetColor(x, y ,YELLOW);
+			}
+		}
+	}
+	led.Update();
+	delay(1000);
+
+
+	led.ClearAll();
+	for (int x = 0; x < led.kWidth; x++){
+		for (int y = 0; y < led.kHeight; y++){
+			if(One[y][x]){
+				led.SetColor(x, y ,MAGENTA);
+			}
+		}
+	}
+	led.Update();
+	delay(1000);
+
 }
+
+
