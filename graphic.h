e266a4917898c428005db7999f1b0235cce6cb37
Shonosuke ISHIWATARI <w@sshoi.com>
2013-01-29 17:34:48 +0900

    add graphic.h

--- graphic.h
+++ graphic.h
@@ -1,11 +1,18 @@
 #ifndef _GRAPHIC_H_
 #define _GRAPHIC_H_
 
+#include "LED.h"
+
 class Graphic {
 private:
-    Graphic();
+	static const bool One[12][6];
+	static const bool Two[12][6];
+	static const bool Three[12][6];
+
 
 public:
+	void CountDown(LED led);
+
 
 };
 
